66c9fa9af5fb9285f3b2978fa96bbb4ecfb6609d
de_gentlem4n <th3g3n3lm4n@gmail.com>
2021-03-25 20:09:52 -0400

    wip

---
+++ api/whatIsLife-model.js
@@ -0,0 +1,29 @@
+
+
+const getAll = () => {
+  return null
+}
+
+const getById = (id) => {
+  return null
+}
+
+const insert = (friend) => {
+  return null
+}
+
+const update = (id, changes) => {
+  return null
+}
+
+const remove = (id) => {
+  return null
+}
+
+module.exports = {
+  getAll,
+  getById,
+  insert,
+  update,
+  remove,
+}
